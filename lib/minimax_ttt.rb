91649b4f2db14a321d305982e7a61a2b48b4eecc
LNA <LAllen@8thLight.com>
2014-05-07 15:45:00 -0500

    Inserts board into same file as Minimax test class

--- lib/minimax_ttt.rb
+++ lib/minimax_ttt.rb
@@ -3,3 +3,19 @@ class Minimax_ttt
     puts "hi"
   end
 end
+
+class Board
+  attr_accessor :spaces, :open_spaces, :fill
+
+  def initialize
+    @spaces = [nil]*9
+  end
+
+  def fill(move, game_piece)
+    @spaces[move.to_i] = game_piece
+  end
+
+  def open_spaces
+   open_spaces =  @spaces.size.times.select {|i| @spaces[i] == nil}
+  end
+end
